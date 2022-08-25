f128fce619250fd1ac81f6e014e44e24f29b63f0
noy <noy.yesodot@gmail.com>
2022-08-25 11:06:55 +0300

    feat: adding mongoose connection

--- Util/mongooseConnection.ts
+++ Util/mongooseConnection.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import configFile from "../config";
 
-const mongooseConnection = async () => {    
+const mongooseConnection = () => {    
   if (mongoose.connection.readyState === 1) return;
-  await mongoose.connect(configFile.mongooseConnection);
+  return mongoose.connect(configFile.mongooseConnection);
 };
 
 export default mongooseConnection;
