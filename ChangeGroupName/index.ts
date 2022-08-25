5aebb3da337d8040a7eda073445d812a58d67cb2
noy <noy.yesodot@gmail.com>
2022-08-25 11:35:03 +0300

    feat: create ChangeGroupName

---
+++ ChangeGroupName/index.ts
@@ -0,0 +1,41 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { MyGroup, nameLength } from "../Group/GroupInterface";
+import GroupRepository from "../Group/GroupRepository";
+import { validateId } from "../Person/PersonInterface";
+import CustomError from "../Util/customError";
+import errorHandler from "../Util/errorHandling";
+import mongooseConnection from "../Util/mongooseConnection";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const id = context.bindingData.id;
+    validateId({ id: id });
+
+    const newName = req.body?.name;
+
+    const validation = nameLength.validate({ name: newName });
+    if (validation.error) throw new CustomError(validation.error.message, 400);
+
+    await mongooseConnection();
+    const group = await MyGroup.findByIdAndUpdate(
+      id,
+      { name: newName },
+      { new: true }
+    );
+    if (group === null)
+      throw new CustomError("There is no such group with given id", 404);
+
+    context.res = {
+      status: 200,
+      body: group
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
+};
+
+export default httpTrigger;
