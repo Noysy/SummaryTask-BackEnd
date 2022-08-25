1253198d0807f3ba8caae2955b0137c5ec7f88a5
noy <noy.yesodot@gmail.com>
2022-08-25 11:29:37 +0300

    feat:  create RemovePersonFromGroup

---
+++ RemovePersonFromGroup/index.ts
@@ -0,0 +1,40 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { MyGroup } from "../Group/GroupInterface";
+import GroupManager from "../Group/GroupManager";
+import GroupRepository from "../Group/GroupRepository";
+import { validateId } from "../Person/PersonInterface";
+import CustomError from "../Util/customError";
+import errorHandler from "../Util/errorHandling";
+import mongooseConnection from "../Util/mongooseConnection";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  _req: HttpRequest
+): Promise<void> {
+  try {
+    const id = context.bindingData.id;
+    validateId({ id: id });
+    const personId = context.bindingData.personId;
+    validateId({ id: personId });
+
+    await mongooseConnection();
+    await GroupManager.getGroup(id);
+
+    if ((await GroupRepository.isPersonInGroup(id, personId)) === null)
+      throw new CustomError("There is no such person in the group", 404);
+
+    context.res = {
+      status: 200,
+      body: await MyGroup.findByIdAndUpdate(
+        id,
+        { $pull: { people: personId } },
+        { new: true }
+      ),
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
+};
+
+export default httpTrigger;
