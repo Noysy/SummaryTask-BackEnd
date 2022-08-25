862a51e2f332fa183a5bb7ea6c5f5b9e392138c1
noy <noy.yesodot@gmail.com>
2022-08-25 11:30:42 +0300

    feat: create GetPersonGroups

---
+++ GetPersonsGroups/index.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { MyGroup } from "../Group/GroupInterface";
+import { validateId } from "../Person/PersonInterface";
+import errorHandler from "../Util/errorHandling";
+import mongooseConnection from "../Util/mongooseConnection";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  _req: HttpRequest
+): Promise<void> {
+  try {
+    const id = context.bindingData.id;
+    validateId({ id: id });
+
+    await mongooseConnection();
+    context.res = {
+      body: await MyGroup.find({ people: id }),
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
+};
+
+export default httpTrigger;
