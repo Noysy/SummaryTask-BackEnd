c4dd919f4005deaa2ba86912b34db50ccd476430
noy <noy.yesodot@gmail.com>
2022-08-25 11:33:06 +0300

    feat: create CreateGroup

---
+++ CreateGroup/index.ts
@@ -0,0 +1,36 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Group, groupRequirements, MyGroup } from "../Group/GroupInterface";
+import CustomError from "../Util/customError";
+import errorHandler from "../Util/errorHandling";
+import mongooseConnection from "../Util/mongooseConnection";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const group: Group = {
+      name: req.body?.name,
+      people: req.body?.people,
+      parentGroup: req.body?.parentGroup,
+    };
+    const validation = groupRequirements.validate(group);
+    if (validation.error) throw new CustomError(validation.error.message, 400);
+
+    await mongooseConnection();
+
+    context.res = {
+      status: 200,
+      body: await MyGroup.create({
+        name: group.name,
+        people: group.people,
+        parentGroup: group.parentGroup,
+      }),
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
+};
+
+export default httpTrigger;
