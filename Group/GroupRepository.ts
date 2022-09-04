5aea775fed4bbec84985e3102307e5aa17164150
noy <noy.yesodot@gmail.com>
2022-09-04 13:06:34 +0300

    feat: add addPersonToGroup function

--- Group/GroupRepository.ts
+++ Group/GroupRepository.ts
@@ -8,9 +8,17 @@ class GroupRepository {
   static isPersonInGroup(id: string, personId: string) {
     return MyGroup.findOne({ people: personId, _id: id });
   }
-  
+
   static async doesGroupHaveParent(id: string) {
     return await MyGroup.findOne({ _id: id, parentGroup: { $exists: true } });
+  }
+
+  static async addPersonToGroup(groupId: string, personId: string) {
+    await MyGroup.findByIdAndUpdate(
+      groupId,
+      { $push: { people: personId } },
+      { new: true }
+    );
   }
 }
 
