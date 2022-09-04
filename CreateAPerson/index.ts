f506589a30c3d291b9b6b1dbaf4583f3a6be43c7
noy <noy.yesodot@gmail.com>
2022-09-04 13:04:25 +0300

    feat: adds person to group in queue

--- CreateAPerson/index.ts
+++ CreateAPerson/index.ts
@@ -1,12 +1,24 @@
 import { AzureFunction, Context } from "@azure/functions";
+import { MyGroup } from "../Group/GroupInterface";
+import errorHandler from "../Util/errorHandling";
 
 const queueTrigger: AzureFunction = async function (
   context: Context,
   _myQueueItem: string
 ): Promise<void> {
-  console.log("all", context.bindings.bothIds);
-  const personId = context.bindings.bothIds.person;
-  const groupId = context.bindings.bothIds.group;
+  try {
+    context.res = {
+      status: 200,
+      body: await MyGroup.findByIdAndUpdate(
+        context.bindings.bothIds.group,
+        { $push: { people: context.bindings.bothIds.person } },
+        { new: true }
+      ),
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
 };
 
 export default queueTrigger;
