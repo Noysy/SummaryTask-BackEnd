636ee0c28f059a2baf3886d757452be51baa9b59
noy <noy.yesodot@gmail.com>
2022-09-07 15:09:43 +0300

    feat: removes person from group when deleting

--- DeletePerson/index.ts
+++ DeletePerson/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { MyGroup } from "../Group/GroupInterface";
 import { validateId, MyPerson } from "../Person/PersonInterface";
 import CustomError from "../Util/customError";
 import errorHandler from "../Util/errorHandling";
@@ -16,6 +17,13 @@ const httpTrigger: AzureFunction = async function (
     const person = await MyPerson.findByIdAndDelete(id);
     if (person === null)
       throw new CustomError("There is no such person with given id", 404);
+
+    (await MyGroup.find({ people: id }, { _id: 1 })).forEach(
+      async (groupId) => {
+        console.log(groupId);
+        await MyGroup.findByIdAndUpdate(groupId, { $pull: { people: id } });
+      }
+    );
 
     context.res = {
       body: person,
