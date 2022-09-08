8e9d90ef8714957bf796508d792fd6a3216b01f4
noy <noy.yesodot@gmail.com>
2022-09-08 17:04:32 +0300

    feat: add file to person

---
+++ addFileToPerson/index.ts
@@ -0,0 +1,90 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import {
+  BlobSASPermissions,
+  BlobServiceClient,
+  generateBlobSASQueryParameters,
+  SASProtocol,
+  StorageSharedKeyCredential,
+} from "@azure/storage-blob";
+import { MyPerson } from "../Person/PersonInterface";
+import PersonManager from "../Person/PersonManager";
+import errorHandler from "../Util/errorHandling";
+import mongooseConnection from "../Util/mongooseConnection";
+import multipart from "parse-multipart";
+import CustomError from "../Util/customError";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const id = context.bindingData.id;
+    await mongooseConnection();
+    await PersonManager.getPerson(id);
+
+    const bodyBuffer = Buffer.from(req.body);
+    const boundary = multipart.getBoundary(req.headers["content-type"]);
+    const data = multipart.Parse(bodyBuffer, boundary)[0];
+    if (
+      await MyPerson.findOne({
+        _id: id,
+        files: { $elemMatch: { name: `${id}-${data.filename}` } },
+      })
+    )
+      throw new CustomError("You can't upload the same image! smh", 400);
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      process.env.AzureWebJobsStorage
+    );
+
+    const containerName = "people-documents";
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    const blockBlobClient = containerClient.getBlockBlobClient(
+      `${id}-${data.filename}`
+    );
+
+    const response = await blockBlobClient.uploadData(data.data, {
+      blobHTTPHeaders: {
+        blobContentType: data.type,
+      },
+    });
+    if (response._response.status !== 201) {
+      throw new CustomError(
+        `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`,
+        500
+      );
+    }
+
+    const blobName = `${id}-${data.filename}`;
+    const storageAccount = "rgnoyhafifa802e";
+
+    const SASToken = generateBlobSASQueryParameters(
+      {
+        containerName: containerName,
+        blobName: blobName,
+        permissions: BlobSASPermissions.parse("racwd"),
+        expiresOn: new Date(new Date().valueOf() + 86400),
+        protocol: SASProtocol.HttpsAndHttp,
+      },
+      new StorageSharedKeyCredential(
+        storageAccount,
+        "hBPdC5g+vpMCWxM+fvB9NGcqZjAdZ/HKmRQhOXD5PY3IqJlk6ZGzCdorihZNFaVeY9n9c9kzrCUW+AStomyDqw=="
+      )
+    ).toString();
+    const SASUrl = `https://${storageAccount}.blob.core.windows.net/${containerName}/${blobName}?${SASToken}`;
+    await MyPerson.findByIdAndUpdate(id, {
+      $push: { files: { name: `${id}-${data.filename}`, url: SASUrl } },
+    });
+
+    context.res = {
+      status: 200,
+      body: { name: data.filename, url: SASUrl },
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
+};
+
+export default httpTrigger;
