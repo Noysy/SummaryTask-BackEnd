a6be73fdfa5445b1b673c93fb748cdbabe36a731
Noy <noyzvi101@gmail.com>
2022-09-18 17:41:41 +0300

    feat: add user limitations

--- GetGroup/index.ts
+++ GetGroup/index.ts
@@ -1,20 +1,25 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import configFile from "../config";
 import GroupManager from "../Group/GroupManager";
-import { validateId } from "../Person/PersonInterface";
+import { DBPerson, validateId } from "../Person/PersonInterface";
+import { authWrapper, userPerm } from "../Util/authorization";
 import errorHandler from "../Util/errorHandling";
 import mongooseConnection from "../Util/mongooseConnection";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
-  _req: HttpRequest
+  _req: HttpRequest,
+  user: DBPerson
 ): Promise<void> {
   try {
     const id = context.bindingData.id;
     validateId({ id: id });
+    const group = await GroupManager.getGroup(id);
+    if (!group.people.includes(user.id) && user.role === 'USER') throw configFile.noPermissionErr;
 
     await mongooseConnection();
     context.res = {
-      body: await GroupManager.getGroup(id),
+      body: group,
     };
   } catch (err) {
     err.statusCode ??= 500;
@@ -22,4 +27,4 @@ const httpTrigger: AzureFunction = async function (
   }
 };
 
-export default httpTrigger;
+export default authWrapper(httpTrigger, userPerm);
