d2637a8510cce04ebf74b7c6394fdabc7a9786a5
Noy <noyzvi101@gmail.com>
2022-09-18 16:15:56 +0300

    feat: add user limitations

--- GetAllPeople/index.ts
+++ GetAllPeople/index.ts
@@ -1,18 +1,36 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { MyPerson, Person } from "../Person/PersonInterface";
+import { MyGroup } from "../Group/GroupInterface";
+import { MyPerson, DBPerson } from "../Person/PersonInterface";
+import { authWrapper, userPerm } from "../Util/authorization";
 import errorHandler from "../Util/errorHandling";
 import mongooseConnection from "../Util/mongooseConnection";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
-  req: HttpRequest,
-  user: Person
+  _req: HttpRequest,
+  user: DBPerson
 ): Promise<void> {
   try {
+    const id = user.id;
+
+    const people = [];
     await mongooseConnection();
+    const groupsOfPerson = await MyGroup.find(
+      { people: id },
+      { _id: 0, people: 1 }
+    ).populate("people");
+    groupsOfPerson.forEach((group) => {
+      group.people.forEach((person) => {
+        if (!people.includes(person)) {
+          people.push(person);
+        }
+      });
+    });
+
+    if (people.length === 0) people.push(await MyPerson.findById(id));
 
     context.res = {
-      body: await MyPerson.find({}),
+      body: [people],
     };
   } catch (err) {
     err.statusCode ??= 500;
@@ -20,4 +38,4 @@ const httpTrigger: AzureFunction = async function (
   }
 };
 
-export default httpTrigger;
+export default authWrapper(httpTrigger, userPerm);
