5a40d010079d60b187716d9955ad3a6ae6bf529c
Noy <noyzvi101@gmail.com>
2022-09-18 17:42:13 +0300

    feat: add user limitations

--- groupDetails/index.ts
+++ groupDetails/index.ts
@@ -1,22 +1,33 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import configFile from "../config";
 import { MyGroup } from "../Group/GroupInterface";
-import { validateId } from "../Person/PersonInterface";
+import { DBPerson, validateId } from "../Person/PersonInterface";
+import { authWrapper, userPerm } from "../Util/authorization";
 import errorHandler from "../Util/errorHandling";
 import mongooseConnection from "../Util/mongooseConnection";
 
-const httpTrigger: AzureFunction = async function (context: Context, _req: HttpRequest): Promise<void> {
-    try {
-        const id = context.bindingData.id;
-        validateId({ id: id });
-    
-        await mongooseConnection();
-        context.res = {
-          body: await MyGroup.find({ _id: id }).populate('people'),
-        };
-      } catch (err) {
-        err.statusCode ??= 500;
-        errorHandler(context, err);
-      }
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  _req: HttpRequest,
+  user: DBPerson
+): Promise<void> {
+  try {
+    const id = context.bindingData.id;
+    validateId({ id: id });
+
+    await mongooseConnection();
+    const group = await MyGroup.findById(id);
+
+    if (!group.people.includes(user.id) && user.role === "USER")
+      throw configFile.noPermissionErr;
+      
+    context.res = {
+      body: group.populate("people"),
+    };
+  } catch (err) {
+    err.statusCode ??= 500;
+    errorHandler(context, err);
+  }
 };
 
-export default httpTrigger;
+export default authWrapper(httpTrigger, userPerm);
