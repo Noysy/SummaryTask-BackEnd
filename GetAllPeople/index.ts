802ebc2d3d41a547b2a48b4aedae1d301774d4ef
Noy <noyzvi101@gmail.com>
2022-09-18 12:38:04 +0300

    fix: delete all jwt things

--- GetAllPeople/index.ts
+++ GetAllPeople/index.ts
@@ -2,15 +2,12 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { MyPerson } from "../Person/PersonInterface";
 import errorHandler from "../Util/errorHandling";
 import mongooseConnection from "../Util/mongooseConnection";
-import { jwtDecode, anyAuth } from "../Util/authorization";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   try {
-    anyAuth(jwtDecode(req.headers["authorization"]));
-
     await mongooseConnection();
 
     context.res = {
