3d4d4371bc06bf289769cd434907e706977a0a0e
Noy <noyzvi101@gmail.com>
2022-09-18 17:41:32 +0300

    feat: add user limitations

--- GetAllPeople/index.ts
+++ GetAllPeople/index.ts
@@ -11,27 +11,33 @@ const httpTrigger: AzureFunction = async function (
   user: DBPerson
 ): Promise<void> {
   try {
-    const id = user.id;
-
-    const people = [];
     await mongooseConnection();
-    const groupsOfPerson = await MyGroup.find(
-      { people: id },
-      { _id: 0, people: 1 }
-    ).populate("people");
-    groupsOfPerson.forEach((group) => {
-      group.people.forEach((person) => {
-        if (!people.includes(person)) {
-          people.push(person);
-        }
+    if (user.role === "USER") {
+      const id = user.id;
+
+      const people = [];
+      const groupsOfPerson = await MyGroup.find(
+        { people: id },
+        { _id: 0, people: 1 }
+      ).populate("people");
+      groupsOfPerson.forEach((group) => {
+        group.people.forEach((person) => {
+          if (!people.includes(person)) {
+            people.push(person);
+          }
+        });
       });
-    });
 
-    if (people.length === 0) people.push(await MyPerson.findById(id));
+      if (people.length === 0) people.push(await MyPerson.findById(id));
 
-    context.res = {
-      body: people,
-    };
+      context.res = {
+        body: people,
+      };
+    } else if (user.role === "ADMIN") {
+      context.res = {
+        body: await MyPerson.find({}),
+      };
+    }
   } catch (err) {
     err.statusCode ??= 500;
     errorHandler(context, err);
